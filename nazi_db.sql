1c9c0c3df7cf07ce4aa57a420daa84ba12281c0d
ABHISEK <abhisekmaheswaran@gmail.com>
2026-06-28 12:01:28 +0530

    Delete nazi_db.sql

--- nazi_db.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE users(
-id INT AUTO_INCREMENT PRIMARY KEY,
-first_name VARCHAR(50),
-last_name VARCHAR(50),
-email VARCHAR(100),
-password VARCHAR(255),
-gender VARCHAR(10),
-dob DATE
-);
